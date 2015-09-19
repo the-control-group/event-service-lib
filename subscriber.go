9f81786e037cea041dcfe5c2c4a8d65efa0b173e
Nick Carenza <nick.carenza@thecontrolgroup.com>
2015-09-19 11:10:03 -0700

    logging listening address

--- subscriber.go
+++ subscriber.go
@@ -6,7 +6,6 @@ import (
 
 func Subscribe(_ logger, nc *nats.Conn, subject, queueGroup string, handler nats.MsgHandler) (subscription *nats.Subscription, err error) {
 	// Listen to all events so that the interesting events can be changed without un/resubscribing
-	var subject = subject
 	subscription, err = nc.QueueSubscribe(subject, queueGroup, handler)
 	if err != nil {
 		return
