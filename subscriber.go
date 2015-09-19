3bec2d9b1b935850e1dad5842b01b1ec90b0f979
Nick Carenza <nick.carenza@thecontrolgroup.com>
2015-09-18 18:39:15 -0700

    made subscribe return

--- subscriber.go
+++ subscriber.go
@@ -5,39 +5,29 @@ import (
 	"time"
 )
 
-var subscription *nats.Subscription
-
-func Subscribe(nc *nats.Conn, subscriberPrefix, queueGroup string, handler nats.MsgHandler) {
-	for {
+func Subscribe(nc *nats.Conn, subscriberPrefix, queueGroup string, handler nats.MsgHandler) (subscription *nats.Subscription) {
+	for subscription == nil {
 		select {
 		case <-Done:
-			subscription.Unsubscribe()
 			return
 		default:
-			for !nc.IsClosed() && (subscription == nil || !subscription.IsValid()) {
-				select {
-				case <-Done:
-					subscription.Unsubscribe()
-					return
-				default:
-					// Listen to all events so that the interesting events can be changed without un/resubscribing
-					var queue = subscriberPrefix + "*.>"
-					var err error
-					if Log != nil {
-						Log.Info("Subscribing to events")
-					}
-					subscription, err = nc.QueueSubscribe(queue, queueGroup, handler)
-					if err != nil {
-						if Log != nil {
-							Log.Error("Unable to subscribe to events. Retrying in 5s.", err)
-						}
-						time.Sleep(5 * time.Second)
-						continue
-					}
-					Log.Info("Subscribed to events")
-				}
+			// Listen to all events so that the interesting events can be changed without un/resubscribing
+			var queue = subscriberPrefix + "*.>"
+			var err error
+			if Log != nil {
+				Log.Info("Subscribing to events")
 			}
-			time.Sleep(5 * time.Second)
+			subscription, err = nc.QueueSubscribe(queue, queueGroup, handler)
+			if err != nil {
+				if Log != nil {
+					Log.Error("Unable to subscribe to events. Retrying in 5s.", err)
+				}
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			Log.Info("Subscribed to events")
+			break
 		}
 	}
+	return
 }
