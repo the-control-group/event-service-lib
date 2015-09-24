3bb56d97915e2242a1451e52d9169504946b8629
Nick Carenza <nick.carenza@thecontrolgroup.com>
2015-09-23 21:58:57 -0700

    added HandleMessages

--- gnatsd.go
+++ gnatsd.go
@@ -5,9 +5,6 @@ import (
 	"net"
 )
 
-/*
-	Should retry indefinitely to establish connection and should reconnect indefinitely if connection is lost
-*/
 func NewGnatsConnection(_ logger, addr *Address) (nc *nats.Conn, err error) {
 	var opts = nats.DefaultOptions
 	opts.Servers = []string{"nats://" + net.JoinHostPort(addr.Host, addr.Port)}
--- subscriber.go
+++ subscriber.go
@@ -2,13 +2,48 @@ package lib
 
 import (
 	"github.com/the-control-group/nats"
+	"sync"
+	"time"
 )
 
 func Subscribe(_ logger, nc *nats.Conn, subject, queueGroup string) (subscription *nats.Subscription, err error) {
-	// Listen to all events so that the interesting events can be changed without un/resubscribing
 	subscription, err = nc.QueueSubscribeSync(subject, queueGroup)
 	if err != nil {
 		return
 	}
 	return
+}
+
+func HandleMessages(log logger, wg *sync.WaitGroup, subscription *nats.Subscription, handler nats.MsgHandler) {
+	var msg *nats.Msg
+	var err error
+	var q int
+	wg.Add(1)
+	defer wg.Done()
+	for {
+		if !subscription.IsActive() {
+			q, err = subscription.QueuedMsgs()
+			if err != nil {
+				log.Warn(err)
+			}
+			if q <= 0 {
+				break
+			}
+		}
+		msg, err = subscription.NextMsg(1 * time.Second)
+		if err != nil {
+			switch err {
+			case nats.ErrTimeout:
+				// Ignore
+			case nats.ErrNoMessages:
+				break
+			default:
+				log.Warn("Unable to read next message")
+			}
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		handler(msg)
+	}
+	log.Debug("Done handling messages")
 }
