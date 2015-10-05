7307c9b8b25853b4c85139ad2393b5ce0b484bd9
Nick Carenza <nick.carenza@thecontrolgroup.com>
2015-10-05 13:34:19 -0700

    added lock for signal handlers to prevent race conditions

--- signals.go
+++ signals.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"os"
+	"sync"
 )
 
 var SigChan = make(chan os.Signal, 1)
@@ -12,13 +13,17 @@ func init() {
 
 var SignalHandlers = map[os.Signal][]func(){}
 
+var SignalLock sync.Mutex
+
 func signalHandler(sigChan chan os.Signal) {
 	for {
 		sig := <-sigChan
+		SignalLock.Lock()
 		if fns, ok := SignalHandlers[sig]; ok {
 			for _, fn := range fns {
 				fn()
 			}
 		}
+		SignalLock.Unlock()
 	}
 }
