243ca0d8ed97af28c810a3921062bcb77840b7c9
Nick Carenza <nick.carenza@thecontrolgroup.com>
2015-11-04 10:30:35 -0800

    added stringer method to lib.address

--- config.go
+++ config.go
@@ -1,5 +1,9 @@
 package lib
 
+import (
+	"net"
+)
+
 type Emitter struct {
 	Address
 	Interval int    `json:"interval"`
@@ -15,6 +19,10 @@ type Listener struct {
 type Address struct {
 	Host string `json:"host"`
 	Port string `json:"port"`
+}
+
+func (addr Address) String() string {
+	return net.JoinHostPort(addr.Host, addr.Port)
 }
 
 type Credentials struct {
